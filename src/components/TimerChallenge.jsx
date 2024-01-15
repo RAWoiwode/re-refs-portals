80250f69afcb77a48b57611cf5bba7f1e674e9f1
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-15 10:37:18 -0600

    Changing the win/lost logic to show corresponding messages

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -4,40 +4,52 @@ import ResultModal from "./ResultModal";
 // let timer; Issues since variable is shared acroos component instances
 
 const TimerChallenge = ({ title, targetTime }) => {
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timerStarted, setTimerStarted] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
   const timer = useRef();
   const dialog = useRef();
 
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  const handleReset = () => {
+    setTimeRemaining(targetTime * 1000);
+  };
+
   const handleStart = () => {
-    setTimerStarted(true);
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      handleStop();
-      dialog.current.open();
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 10);
+    }, 10);
   };
 
   const handleStop = () => {
-    setTimerStarted(false);
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   };
 
   return (
     <>
-      <ResultModal result={"lost"} targetTime={targetTime} ref={dialog} />
+      <ResultModal
+        remainingTime={timeRemaining}
+        targetTime={targetTime}
+        ref={dialog}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Timer is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Timer is running..." : "Timer inactive"}
         </p>
       </section>
     </>
